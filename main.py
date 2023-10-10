67ee0d22c16f89f367e02a768c29e6dce4277f85
maicon <maiconmirandasantana@gmail.com>
2023-10-09 22:25:17 -0300

    Refatorando codigo

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from cadastroServico import CadastroServico
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Barber Shop")
+        self.setWindowTitle("Studio Beard")
         self.resize(750, 650)
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowMaximizeButtonHint)
         self.setWindowIcon(QtGui.QIcon("assets/iconBarber.ico"))
@@ -58,38 +58,91 @@ class MainWindow(QMainWindow):
 
         # ------------------------------------------------------
 
-        self.lblCadastro = QLabel(self)
-        self.lblCadastro.setGeometry(160, 410, 150, 180)
-        self.lblCadastro.setStyleSheet("background-color: #282828; border-radius: 5px;")
-        self.lblTextCadastro = QLabel(self)
-        self.lblTextCadastro.setText("Cadastro")
-        self.lblTextCadastro.setGeometry(190, 470, 100, 180)
-        self.lblTextCadastro.setStyleSheet("font-size: 30px; color: #fff; font-family: 'Great Vibes', cursive;")
+        verify = 1
 
-        self.imgUser = QLabel(self)
-        self.imgUser.setGeometry(200, 450, 350, 100)
-        self.pixmap = QPixmap('assets/imgUser.png')
-        self.pixmap = self.pixmap.scaled(75, 75)
-        self.imgUser.setPixmap(self.pixmap)
-        self.imgUser.resize(self.pixmap.width(), self.pixmap.height())
-        self.imgUser.mousePressEvent = self.pressCadastro
+        try:
+            if verify == 1:
+                self.lblCadastro = QLabel(self)
+                self.lblCadastro.setGeometry(160, 410, 150, 180)
+                self.lblCadastro.setStyleSheet("background-color: #282828; border-radius: 5px;")
+                self.lblTextCadastro = QLabel(self)
+                self.lblTextCadastro.setText("Cadastro")
+                self.lblTextCadastro.setGeometry(190, 470, 100, 180)
+                self.lblTextCadastro.setStyleSheet("font-size: 30px; color: #fff; font-family: 'Great Vibes', cursive;")
 
-        # ------------------------------------------------------
+                self.imgUser = QLabel(self)
+                self.imgUser.setGeometry(200, 450, 350, 100)
+                self.pixmap = QPixmap('assets/imgUser.png')
+                self.pixmap = self.pixmap.scaled(75, 75)
+                self.imgUser.setPixmap(self.pixmap)
+                self.imgUser.resize(self.pixmap.width(), self.pixmap.height())
+                self.imgUser.mousePressEvent = self.pressCadastro
 
-        self.lblAbout = QLabel(self)
-        self.lblAbout.setGeometry(450, 410, 150, 180)
-        self.lblAbout.setStyleSheet("background-color: #282828; border-radius: 5px;")
-        self.lblTextAbout = QLabel(self)
-        self.lblTextAbout.setText("Sobre nós")
-        self.lblTextAbout.setGeometry(475, 470, 130, 180)
-        self.lblTextAbout.setStyleSheet("font-size: 30px; color: #fff; font-family: 'Great Vibes', cursive;")
+                # ------------------------------------------------------
 
-        self.imgAbout = QLabel(self)
-        self.imgAbout.setGeometry(490, 450, 350, 100)
-        self.pixmap = QPixmap('assets/imgAbout.png')
-        self.pixmap = self.pixmap.scaled(75, 75)
-        self.imgAbout.setPixmap(self.pixmap)
-        self.imgAbout.resize(self.pixmap.width(), self.pixmap.height())
+                self.lblAbout = QLabel(self)
+                self.lblAbout.setGeometry(450, 410, 150, 180)
+                self.lblAbout.setStyleSheet("background-color: #282828; border-radius: 5px;")
+                self.lblTextAbout = QLabel(self)
+                self.lblTextAbout.setText("Sobre nós")
+                self.lblTextAbout.setGeometry(475, 470, 130, 180)
+                self.lblTextAbout.setStyleSheet("font-size: 30px; color: #fff; font-family: 'Great Vibes', cursive;")
+
+                self.imgAbout = QLabel(self)
+                self.imgAbout.setGeometry(490, 450, 350, 100)
+                self.pixmap = QPixmap('assets/imgAbout.png')
+                self.pixmap = self.pixmap.scaled(75, 75)
+                self.imgAbout.setPixmap(self.pixmap)
+                self.imgAbout.resize(self.pixmap.width(), self.pixmap.height())
+            else:
+                self.lblCadastro = QLabel(self)
+                self.lblCadastro.setGeometry(300, 410, 150, 180)
+                self.lblCadastro.setStyleSheet("background-color: #282828; border-radius: 5px;")
+                self.lblTextCadastro = QLabel(self)
+                self.lblTextCadastro.setText("Cadastro")
+                self.lblTextCadastro.setGeometry(330, 470, 100, 180)
+                self.lblTextCadastro.setStyleSheet("font-size: 30px; color: #fff; font-family: 'Great Vibes', cursive;")
+
+                self.imgUser = QLabel(self)
+                self.imgUser.setGeometry(343, 450, 350, 100)
+                self.pixmap = QPixmap('assets/imgUser.png')
+                self.pixmap = self.pixmap.scaled(75, 75)
+                self.imgUser.setPixmap(self.pixmap)
+                self.imgUser.resize(self.pixmap.width(), self.pixmap.height())
+                self.imgUser.mousePressEvent = self.pressCadastro
+        except Exception as e:
+            self.lblCadastro = QLabel(self)
+            self.lblCadastro.setGeometry(160, 410, 150, 180)
+            self.lblCadastro.setStyleSheet("background-color: #282828; border-radius: 5px;")
+            self.lblTextCadastro = QLabel(self)
+            self.lblTextCadastro.setText("Cadastro")
+            self.lblTextCadastro.setGeometry(190, 470, 100, 180)
+            self.lblTextCadastro.setStyleSheet("font-size: 30px; color: #fff; font-family: 'Great Vibes', cursive;")
+
+            self.imgUser = QLabel(self)
+            self.imgUser.setGeometry(200, 450, 350, 100)
+            self.pixmap = QPixmap('assets/imgUser.png')
+            self.pixmap = self.pixmap.scaled(75, 75)
+            self.imgUser.setPixmap(self.pixmap)
+            self.imgUser.resize(self.pixmap.width(), self.pixmap.height())
+            self.imgUser.mousePressEvent = self.pressCadastro
+
+            # ------------------------------------------------------
+
+            self.lblAbout = QLabel(self)
+            self.lblAbout.setGeometry(450, 410, 150, 180)
+            self.lblAbout.setStyleSheet("background-color: #282828; border-radius: 5px;")
+            self.lblTextAbout = QLabel(self)
+            self.lblTextAbout.setText("Sobre nós")
+            self.lblTextAbout.setGeometry(475, 470, 130, 180)
+            self.lblTextAbout.setStyleSheet("font-size: 30px; color: #fff; font-family: 'Great Vibes', cursive;")
+
+            self.imgAbout = QLabel(self)
+            self.imgAbout.setGeometry(490, 450, 350, 100)
+            self.pixmap = QPixmap('assets/imgAbout.png')
+            self.pixmap = self.pixmap.scaled(75, 75)
+            self.imgAbout.setPixmap(self.pixmap)
+            self.imgAbout.resize(self.pixmap.width(), self.pixmap.height())
 
     def pressCadastroServico(self, event):
         windowCadService = CadastroServico()
